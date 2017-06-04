4c5eff9e6d9e7d9b71cc39d3fd16505fd576084a
Brad Pizzimenti <brad@gennakersystems.com>
2017-06-04 01:49:28 -0700

    create respawn script to drop player back on map if they fall off

---
+++ Assets/Scripts/MapEdgeRespawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapEdgeRespawn : MonoBehaviour {
+
+	private Vector3 spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+		spawnPoint = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.y < -100)
+			transform.position = spawnPoint;
+	}
+}
