790243e029b397aab6129738d484c0490f12866e
Brad Pizzimenti <brad@gennakersystems.com>
2017-06-03 17:27:33 -0700

    add script for moving forward while GvrClickbutton is active

---
+++ Assets/Scripts/LocomotionWalking.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocomotionWalking : MonoBehaviour
+{
+    public float walkingSpeed = 0.5f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GvrController.ClickButton)
+        {
+            Walk();
+        }
+    }
+
+    private void Walk()
+    {
+        Vector3 velocity = new Vector3(0f, 0f, walkingSpeed);
+        transform.position += Camera.main.transform.forward;
+    }
+}
